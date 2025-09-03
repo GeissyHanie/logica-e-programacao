5673905c36742745afbd45ac31793e22f896a21a
Geissy Araújo <geissyaraujo@gmail.com>
2025-09-03 17:08:02 -0300

    Adiciona função para calcular média e testes de soma e média

--- src/calculadora.js
+++ src/calculadora.js
@@ -4,15 +4,16 @@ function somarDoisNumeros(valor1, valor2) {
 }
 
 function calcularMediaDeDoisNumeros(valor1, valor2) {
-    //somar dois valores
+    
     const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2);
-    //fazer a média
+    
     const resultadoDaMediaDeDoisValores = resultadoSomaDeDoisValores/2;
-    //Retornar resultado
+    
     return resultadoDaMediaDeDoisValores
 }
 
 
-module.exports = { //significa mostrar para o mundo
-    somarDoisNumeros
+module.exports = {
+    somarDoisNumeros,
+    calcularMediaDeDoisNumeros
 }
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,21 +1,60 @@
-const { somarDoisNumeros } = require('../src/calculadora');
+const { somarDoisNumeros, calcularMediaDeDoisNumeros } = require('../src/calculadora');
 const { expect } = require('chai');
 
 describe('Testes da Função de Soma', function () {
     it('A função deve ser capaz de somar dois números positivos', function () {
-        // Coleta o resultado da função
+        
         const resultadoDaSoma = somarDoisNumeros(5, 3);
 
-        // Compara o resultado com o valor que você espera
         expect(resultadoDaSoma).to.equal(8);
     });
 
     it('A função deve ser capaz de somar um número positivo e um negativo', function () {
-        // Coleta o resultado da função
+        
         const resultadoDaSoma = somarDoisNumeros(50, -15);
 
-        // Compara o resultado com o valor que você espera
         expect(resultadoDaSoma).to.equal(35);
+    });
+
+    it('A função deve ser capaz de somar dois números zero', function () {
+        
+        const resultadoDaSoma = somarDoisNumeros(0, 0);
+
+        expect(resultadoDaSoma).to.equal(0);
+    });
+    it('A função deve ser capaz de somar dois números negativos', function () {
+        const resultadoDaSoma = somarDoisNumeros(-2, -3);
+
+        expect(resultadoDaSoma).to.equal(-5);
     });
 });
 
+describe('Testes da Função Calcular Média de Dois Valores', function () {
+    it('A função deve ser capaz de realizar a média dois números positivos', function () {
+        
+        const resultadoDaMediaDeDoisValores = calcularMediaDeDoisNumeros(5, 3);
+
+        const mediaDeDoisNumeros = resultadoDaMediaDeDoisValores/2;
+
+        expect(resultadoDaMediaDeDoisValores).to.equal(4);
+    });
+
+    it('A função deve ser capaz de realizar a média de um número positivo e um negativo', function () {
+        
+        const resultadoDaMediaDeDoisValores = calcularMediaDeDoisNumeros(50, -10);
+
+        expect(resultadoDaMediaDeDoisValores).to.equal(20);
+    });
+
+    it('A função deve ser capaz de realizar a média entre dois zeros', function () {
+        const resultadoDaMediaDeDoisValores = calcularMediaDeDoisNumeros(0, 0);
+
+        expect(resultadoDaMediaDeDoisValores).to.equal(0);
+    });
+
+    it('A função deve ser capaz de realizar a média entre dois números negativos', function () {
+        const resultadoDaMediaDeDoisValores = calcularMediaDeDoisNumeros(-10, -2);
+
+        expect(resultadoDaMediaDeDoisValores).to.equal(-6)
+    });
+});
